a35e475bcd93b00188864a1ef4242d7846a7422d
Geonwoo Shin <dagarn@unopnd.com>
2022-12-12 17:49:43 +0900

    Add Seaport component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Package />
+    <Seaport />
   </div>
 </template>
 
 <script>
 import Package from './components/Package.vue'
+import Seaport from './components/Seaport.vue'
 
 export default {
   name: 'App',
   components: {
-    Package
+    Package,
+    Seaport
   }
 }
 </script>
---
+++ src/components/Seaport.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Test
+  </div>
+</template>
+
+<script>
+import { Seaport } from "@opensea/seaport-js";
+import { ethers } from "ethers";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const seaport = new Seaport(provider);
+
+export default {
+  name: "Seaport",
+  methods: {
+  },
+}
+</script>
+
+<style scoped>
+</style>
